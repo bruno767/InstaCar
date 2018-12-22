f7e367bb0005222f2834602737f7cb07b25b25db
Bruno Alves <bruno.alves@widas.de>
2018-12-22 15:46:48 +0100

    Initial Commit

---
+++ InstaCar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCar
+//
+//  Created by Bruno Alves on 22.12.18.
+//  Copyright © 2018 Bruno Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
